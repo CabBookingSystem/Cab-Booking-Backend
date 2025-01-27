ce89673f86d29d09011b4da8460dabbabd966bc2
86729-harshada <harshada3678@gmail.com>
2025-01-27 22:52:17 +0530

    entities creation

--- spring_boot_backend_template/src/main/java/com/cabBooking/Entities/Address.java
+++ spring_boot_backend_template/src/main/java/com/cabBooking/Entities/Address.java
@@ -2,6 +2,7 @@ package com.cabBooking.Entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,8 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString(callSuper = true)
+@Table(name = "address")
+
 public class Address extends BaseEntity {
 	@Column(name="adr_line1",length=100)
 	private String adrLine1;
@@ -27,4 +30,13 @@ public class Address extends BaseEntity {
 	
 	@Column(length=20,name="pin_code")
 	private String pinCode;
+
+	public Address(String adrLine1, String adrLine2, String city, String state, String pinCode) {
+		super();
+		this.adrLine1 = adrLine1;
+		this.adrLine2 = adrLine2;
+		this.city = city;
+		this.state = state;
+		this.pinCode = pinCode;
+	}
 }
