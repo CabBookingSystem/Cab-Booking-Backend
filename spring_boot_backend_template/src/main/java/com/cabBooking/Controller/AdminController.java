500d9a6fd3adeb1b7dd0c0ca44d42d4595d620ae
KD2-Shivali-86650 <5snshivali@gmail.com>
2025-02-05 18:19:18 +0530

    location added

--- spring_boot_backend_template/src/main/java/com/cabBooking/Controller/AdminController.java
+++ spring_boot_backend_template/src/main/java/com/cabBooking/Controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ import com.cabBooking.Service.UserService;
 
 @RestController
 @RequestMapping("/Admin")
+@CrossOrigin(origins="http://localhost:3000")
 public class AdminController {
 	@Autowired
 	private AdminService  adminService;
